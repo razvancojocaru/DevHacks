d8359c0b13640d59ab44309a881859f78423ea09
Razvan Cojocaru <razvan.cojocaru93@gmail.com>
2016-11-20 05:19:45 +0200

    updated buttons with text

--- ShiC/app/src/main/java/com/shic/shic/MainActivity.java
+++ ShiC/app/src/main/java/com/shic/shic/MainActivity.java
@@ -22,7 +22,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
